f7e8205b7b45f00e88b55a15722909e12c1fccf1
David Casanellas <davidcasanell@gmail.com>
2021-11-07 18:17:01 +0100

    Add disabled, textOnDisabled and sort defaultTheme properties

--- src/utils/themes.js
+++ src/utils/themes.js
@@ -2,11 +2,13 @@ import { blue, neutral } from "./colors";
 import { primaryFont } from "./typography";
 
 export const defaultTheme = {
+  disabled: neutral[400],
   primaryColor: blue[300],
-  primaryColorHover: blue[200],
-  primaryColorActive: blue[100],
-  textColorOnPrimary: neutral[100],
+  primaryActiveColor: blue[100],
+  primaryHoverColor: blue[200],
+  primaryFont,
   textColor: neutral[600],
   textColorInverted: neutral[100],
-  primaryFont,
+  textColorOnPrimary: neutral[100],
+  textOnDisabled: neutral[300],
 };
