86bc768735da7982c582a19ebdb0b852a07cbef6
David Casanellas <davidcasanell@gmail.com>
2021-11-08 01:07:52 +0100

    Rename typeScale header properties

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,11 +1,11 @@
 export const primaryFont = '"Roboto Mono", monospace';
 
 export const typeScale = {
-  header1: "1.8rem",
-  header2: "1.6rem",
-  header3: "1.4rem",
-  header4: "1.2rem",
-  header5: "1.1rem",
+  h1: "1.8rem",
+  h2: "1.6rem",
+  h3: "1.4rem",
+  h4: "1.2rem",
+  h5: "1.1rem",
   paragraph: "1rem",
   helperText: "0.8rem",
   copyrightText: "0.7rem",
